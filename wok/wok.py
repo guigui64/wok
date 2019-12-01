1a8bc7b529b7e4ac7075901a7844215f2f461da4
guigui64 <guillaume.comte10@gmail.com>
2019-12-01 22:32:20 +0100

    wip: job load/save + wok

--- wok/wok.py
+++ wok/wok.py
@@ -1,5 +1,8 @@
 import pathlib
 
+# from wok.job import Job
+# from wok.task import Task
+
 
 class Wok:
 
@@ -9,11 +12,24 @@ class Wok:
         self.jobs = []
         self.current_job = 0
 
-    def load(self, dir=default_dir):
-        """ 
-        for job_dir in dir.iterdir():
-            job = Job(
+    def __check_dir(dir):
+        if dir.exists() and not dir.is_file():
+            print(f"ERROR: {dir} exists and is not a dir!")
+            return False
+        if not dir.exists():
+            dir.mkdir()
+        return True
 
-        def list
-        """
-        pass
+    def load(self, dir=default_dir):
+        if not self.__check_dir(dir):
+            print("Could not load (see previous error)")
+            return False
+        # Now dir exists and is a directory
+        for file in dir.iterdir():
+            pass  # TODO files and folders
+
+    def save(self, dir=default_dir):
+        if not self.__check_dir(dir):
+            print("Could not save (see previous error)")
+            return False
+        # TODO save files
